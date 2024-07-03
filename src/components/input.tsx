d79cbd75c3211cdc10ccdb7753863ca379c5f393
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-07-03 10:39:26 -0300

    fix: remove unecessary import for build process

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from 'react'
+import { ComponentProps } from 'react'
 import { useFormContext } from 'react-hook-form'
 
 interface InputProps extends ComponentProps<'input'> {
