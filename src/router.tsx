54df26e839fe81986bd92a5f8deb2a448cdfbc1e
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-07-02 22:13:55 -0300

    refactor: create "Create" route

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Home } from './pages/Home'
 import { EditApolice } from './pages/Edit'
+import { CreateApolice } from './pages/Create'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateApolice />} />
         <Route path="/edit/:id" element={<EditApolice />} />
         {/* <Route path="*" element={} /> */}
       </Route>
